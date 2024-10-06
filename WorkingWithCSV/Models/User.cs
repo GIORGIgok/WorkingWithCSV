29f031dc51a60951be66b0bd3b5dd84b62ec087b
user <user@WINDOWS-NE2D8Q2>
2024-10-06 21:34:18 +0400

    Add project files.

---
+++ WorkingWithCSV/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithCSV.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string IpAdress { get; set; } = null!;
+
+        public static User ParseWordData(string data)
+        {
+            string[] separatedData = data.Split(',');
+
+            if (separatedData.Length != 5)
+            {
+                throw new FormatException($"Invalid data {data}");
+            }
+
+            User user = new User
+            {
+                Id = int.Parse(separatedData[0]),
+                FirstName = separatedData[1],
+                LastName = separatedData[2],
+                Email = separatedData[3],
+                IpAdress = separatedData[4]
+            };
+            return user;
+        }
+    }
+}
---
+++ WorkingWithCSV/Program.cs
@@ -0,0 +1,16 @@
+﻿using WorkingWithCSV.Models;
+
+namespace WorkingWithCSV
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string[] data = File.ReadAllLines(@"../../../mock_data.csv");
+
+            /// check if it works
+            User parsedData = User.ParseWordData("10,Haydon,Barnshaw,hbarnshaw9@google.nl,152.111.235.194");
+        }
+    }
+}
